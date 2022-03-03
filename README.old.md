b7b42dfc9e60a879dafcabc6e27908788d716716
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-03-03 23:37:21 +0900

    Feat: add pokeapi

---
+++ README.old.md
@@ -0,0 +1,2 @@
+## ⚡pokemon-app
+* React.js를 활용한 미니 프로젝트
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon "
+  );
+
+  useEffect(() => {
+    axios.get(currentPageUrl).then((res) => {
+      setPokemon(res.data.results.map((p) => p.name));
+    });
+  }, [currentPageUrl]);
+
+  return <PokemonList pokemon={pokemon} />;
+}
+
+export default App;
